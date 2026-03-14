dcf896318397fcc5da85cbd1501ea7c0ac8be197
Harry56470 <harryqd23@gmail.com>
2026-03-14 14:31:50 -0700

    worked on library pages

--- lib/pages/library.dart
+++ lib/pages/library.dart
@@ -10,6 +10,23 @@ class LibraryPage extends StatefulWidget {
 class _LibraryPageState extends State<LibraryPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        videoTile(),
+        videoTile(),
+        videoTile(),
+        videoTile(),
+      ],
+    );
+  }
+
+
+  Widget videoTile(){
+    return Row(
+      children: [
+        Image.network("https://static.wikia.nocookie.net/the-snack-encyclopedia/images/7/7d/Apple.png/revision/latest?cb=20200706145821",width: 100,),
+        Text("data")
+      ],
+    );
   }
 }
